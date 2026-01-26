57675ca4de79ba8e068bc26640ff708bc124b07b
maxibarra <maxi.8379@mail.com>
2026-01-26 19:35:17 -0300

    primera parte del desafio resulta, metodos funcionando y los  test pasaron

--- source/models.ts
+++ source/models.ts
@@ -2,11 +2,30 @@
 import "./contacts.json";
 // si no estuviera este import typescript no se da cuenta que lo necesitamos
 // ya que para escribir o leer al archivo usamos la libreria "jsonfile"
-
+import * as jsonfile from "jsonfile";
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const json = jsonfile.readFileSync("source/contacts.json");
+    this.data = json;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact) {
+    this.data.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync(__dirname + "/contacts.json", this.data);
+  }
+  getOneById(id: number) {
+    return this.data.find((contact) => contact.id === id);
+  }
+  
+}
 export { ContactsCollection };
